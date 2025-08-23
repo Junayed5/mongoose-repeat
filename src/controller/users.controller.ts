3c40986180bd4bc2f8be826a3e7ad09e5c9a35f8
Junayed5 <mdjunayed352003@gmail.com>
2025-08-24 00:06:40 +0600

    custom validator and zod for data senitizing

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -1,8 +1,38 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/user.models";
+import z from "zod";
 
 export const userRouter = express.Router();
 
+const createUserByZod = z.object({
+  fir stName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
+
+userRouter.post("/create-user", async (req: Request, res: Response) => {
+  try {
+    const body =  await createUserByZod.parseAsync(req.body);
+    // const users = await User.create(body);
+    console.log(body, 'zod body')
+
+    res.status(201).json({
+      success: true,
+      message: "User added successfully",
+      users: {},
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: "user not found",
+      error,
+    });
+  }
+});
 userRouter.get("/", async (req: Request, res: Response) => {
   const users = await User.find();
 
@@ -23,19 +53,10 @@ userRouter.get("/:userId", async (req: Request, res: Response) => {
     users,
   });
 });
-userRouter.post("/create-user", async (req: Request, res: Response) => {
-  const body = req.body;
-  const users = await User.create(body);
 
-  res.status(201).json({
-    success: true,
-    message: "User added successfully",
-    users,
-  });
-});
 userRouter.patch("/:userId", async (req: Request, res: Response) => {
   const userId = req.params.userId;
-  const body = req.params.body;
+  const body = req.body;
   //   const users = await User.findByIdAndUpdate(userId, body, {new: true})
   const users = await User.findOneAndUpdate({ _id: userId }, body, {
     new: true,
--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,19 +1,34 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interface/user.interface";
+import validator from 'validator'
 
 const userSchema = new Schema<IUser>(
   {
     firstName: {
       type: String,
       required: true,
+      minLength: [3, "First name must be 3 charecters, got {VALUE}"],
+      maxLength: 10
     },
     lastName: {
       type: String,
       required: true,
+      minLength: 3,
+      maxLength: 10
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, 'Age must be 18, got {VALUE}'],
+      max: 60
     },
     email: {
       type: String,
       required: true,
+      unique: [true, "Already have an account, Please Login"],
+      lowercase: true,
+      trim:true,
+      validate: [validator.isEmail, "Email is not valid, got {VALUE}"]
     },
     password: {
       type: String,
@@ -22,8 +37,12 @@ const userSchema = new Schema<IUser>(
     role: {
       type: String,
       required: true,
-      enum: ["user", "admin"],
-      default: "user",
+      enum: {
+        values: ["USER", "ADMIN", "SUPERADMIN"],
+        message: 'Role is not valid, You have to give like : user, admin, superadmin'
+      },
+      default: "USER",
+      uppercase: true
     },
   },
   {
